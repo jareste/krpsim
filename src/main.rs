4b582994308ee96fa49556e07c0e065a48653792
sebamiro <seba.mi.21@gmail.com>
2024-07-20 14:45:14 +0200

    add: Data type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-mod tokens;
+use std::collections::HashMap;
+
 mod lexer;
 mod parser;
+mod tokens;
+
+#[derive(Debug)]
+pub struct Process {
+    id: String,
+    input: Vec<(String, u64)>,
+    output: Vec<(String, u64)>,
+    time: u64,
+}
+
+#[derive(Debug)]
+pub struct Data {
+    pub stocks: HashMap<String, u64>,
+    pub processes: Vec<Process>,
+    pub objectives: Vec<String>,
+}
 
 fn main() {
-    parser::Parser::new("test.txt").parse().unwrap();
+    let mut parser = parser::Parser::new("test.txt");
+    parser.parse().unwrap();
+    let x = Data {
+        stocks: parser.stocks,
+        processes: parser.process,
+        objectives: parser.optimize.unwrap(),
+    };
+    println!("{:?}", x);
 }
