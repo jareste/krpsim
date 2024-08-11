2030a778db8e7d8ac455a81c7f1bd0fd2e6ae612
jareste <jareste2000@gmail.com>
2024-08-11 13:26:05 +0200

    removed chatgpt comments hehe

--- src/gen_file.rs
+++ src/gen_file.rs
@@ -5,14 +5,12 @@ use std::collections::HashMap;
 pub fn generate_log_file(filename: &str, initial_stocks: &HashMap<String, u64>, final_stocks: &HashMap<String, u64>, log: &[(String, u64, u64)], finish_time: u64) -> io::Result<()> {
     let mut file = File::create(filename)?;
 
-    // Write initial stocks
     writeln!(file, "Initial stocks:")?;
     for (stock, amount) in initial_stocks {
         writeln!(file, "{}:{}", stock, amount)?;
     }
     writeln!(file)?;
 
-    // Write the log of actions
     for (process, count, time) in log {
         for _ in 0..*count {
             writeln!(file, "{}:{}", time, process)?;
@@ -20,11 +18,9 @@ pub fn generate_log_file(filename: &str, initial_stocks: &HashMap<String, u64>, 
     }
     writeln!(file)?;
 
-    // Write finish time
     writeln!(file, "Finished at time {}", finish_time)?;
     writeln!(file)?;
 
-    // Write final stocks
     writeln!(file, "Final stocks:")?;
     for (stock, amount) in final_stocks {
         writeln!(file, "{}:{}", stock, amount)?;
