0b0bc3123ce2889c9788ced55b1438fa0ffe85db
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-08-14 17:44:36 +0200

    removed ctrlc

--- src/delay.rs
+++ src/delay.rs
@@ -2,21 +2,10 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Once};
 use std::thread;
 use std::time::Duration;
-use ctrlc;
-
-static INIT: Once = Once::new();
 
 pub fn start_timer(duration: Duration) -> Arc<AtomicBool> {
     let timer_flag = Arc::new(AtomicBool::new(false));
     let flag_clone = Arc::clone(&timer_flag);
-
-    let ctrlc_flag = Arc::clone(&timer_flag);
-    INIT.call_once(|| {
-        ctrlc::set_handler(move || {
-            ctrlc_flag.store(true, Ordering::SeqCst);
-            println!("\x1b[33m\nCtrl-C detected, stopping optimization\n\x1b[0m");
-        }).expect("Error setting Ctrl-C handler");
-    });
 
     thread::spawn(move || {
         thread::sleep(duration);
